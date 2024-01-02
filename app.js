56bc2f1ff4cdf8482d8bc4c730cb6ecb7ff1e895
Satya-S3 <satyasahu644@gmail.com>
2024-01-02 16:42:15 +0530

    Hello

---
+++ app.js
@@ -0,0 +1,39 @@
+let speech =new SpeechSynthesisUtterance();
+
+let voices=[];
+let voiceSelect=document.querySelector("select");
+window.speechSynthesis.onvoiceschanged=()=>{
+      voices=window.speechSynthesis.getVoices();
+      speech.voice=voices[0];
+
+      voices.forEach((voice,i)=>(voiceSelect.options[i]=new Option(voice.name,i)));
+};
+
+voiceSelect.addEventListener("change",()=>{
+      speech.voice=voices[voiceSelect.value];
+})
+document.querySelector("#listen").addEventListener("click",()=>{
+      speech.text=document.querySelector("textarea").value;
+      window.speechSynthesis.speak(speech);
+})
+
+////Speak
+document.querySelector("#speak").addEventListener('click',function(){
+      let speech=true;
+      window.SpeechRecognition=window.webkitSpeechRecognition;
+      const recognition=new SpeechRecognition;
+      recognition.interimResults=true;
+
+      recognition.addEventListener('result',e=>{
+            const transcript=Array.from(e.results)
+            .map(result=>result[0])
+            .map(result=>result.transcript)
+
+            document.querySelector('textarea').innerHTML=transcript;
+      })
+
+      if(speech==true){
+            recognition.start();
+      }
+})
+
